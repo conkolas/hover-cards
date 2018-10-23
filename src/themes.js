319ab8e9806bbba91bab2e9ea98b6096920f212e
conkolas <aistismatulis@gmail.com>
2018-10-23 17:55:08 +0000

    Button with theming

---
+++ src/themes.js
@@ -0,0 +1,16 @@
+export const themes = {
+  default: {
+    bodyBackground: '#fbf9f6',
+
+    button: {
+      color: '#FFFFFF',
+      background: 'transparent',
+      borderColor: '#FFFFFF',
+      hover: {
+        color: '#000000',
+        background: '#FFFFFF',
+      },
+    }
+
+  },
+};
