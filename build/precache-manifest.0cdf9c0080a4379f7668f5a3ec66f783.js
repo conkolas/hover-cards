761b11ca81495eb6ac724b7e8ddf3d03752269fe
conkolas <aistismatulis@gmail.com>
2018-10-23 21:18:28 +0000

    App build

---
+++ build/precache-manifest.0cdf9c0080a4379f7668f5a3ec66f783.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c1e98c88f4ea686bbf3",
+    "url": "/static/js/main.5c1e98c8.chunk.js"
+  },
+  {
+    "revision": "dac50c74cfa08b728887",
+    "url": "/static/js/1.dac50c74.chunk.js"
+  },
+  {
+    "revision": "56b53f89078cf442af5b4d52eab646b5",
+    "url": "/index.html"
+  }
+];
